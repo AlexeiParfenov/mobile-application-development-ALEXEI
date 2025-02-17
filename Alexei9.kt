48960d2fb2a080dd68884ac540c728be613e442f
AlexeiParfenov <parfenovaleksej29@gmail.com>
2025-02-17 13:55:07 +0900

    Delete Alexei9.kt

--- Alexei9.kt
+++
@@ -1,8 +0,0 @@
-fun main(){
-    println("Введите четыре числа: ")
-    val num1 = readLine()!!.toDouble()
-    val num2 = readLine()!!.toDouble()
-    val num3 = readLine()!!.toDouble()
-    val num4 = readLine()!!.toDouble()
-    println("$num1 $num2 $num3 $num4")
-}
